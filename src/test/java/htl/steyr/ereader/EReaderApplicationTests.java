d6ccbeb7f9af6b20a3c32234a024c52de3f91bea
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-02-06 08:36:53 +0100

    Initial commit

---
+++ src/test/java/htl/steyr/ereader/EReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package htl.steyr.ereader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EReaderApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
