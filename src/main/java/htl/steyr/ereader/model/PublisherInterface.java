52c59ad4d76be07959a444d4cf041b327fd96b39
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-03-03 18:23:00 +0100

    feat: add javadoc to publisher/subscriber

--- src/main/java/htl/steyr/ereader/model/PublisherInterface.java
+++ src/main/java/htl/steyr/ereader/model/PublisherInterface.java
@@ -1,5 +1,16 @@
 package htl.steyr.ereader.model;
 
+/**
+ * <h4>Publisher Interface</h4>
+ * <br>
+ * Publisher/Subscriber Pattern for Controllers
+ *
+ * @author Dino Kupinic
+ */
 public interface PublisherInterface {
+  /**
+   * Add Subscriber to Publisher List of Subscribers
+   * @param sub Subscriber
+   */
   void addSubscriber(SubscriberInterface sub);
 }
--- src/main/java/htl/steyr/ereader/model/SubscriberInterface.java
+++ src/main/java/htl/steyr/ereader/model/SubscriberInterface.java
@@ -1,6 +1,17 @@
 package htl.steyr.ereader.model;
 
+/**
+ * <h4>Subscriber Interface</h4>
+ * <br>
+ * Publisher/Subscriber Pattern for Controllers
+ *
+ * @author Dino Kupinic
+ */
 public interface SubscriberInterface {
+  /**
+   * Trigger Action for Subscribers
+   * @author Dino Kupinic
+   */
   void triggerAction();
 }
 
