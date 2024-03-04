738b701b05e9b4fccb6e61d94e32780b008a9221
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-03-04 14:39:49 +0100

    chore: add fxml variables

--- src/main/java/htl/steyr/ereader/controller/Controller.java
+++ src/main/java/htl/steyr/ereader/controller/Controller.java
@@ -1,25 +1,46 @@
 package htl.steyr.ereader.controller;
 
 import htl.steyr.ereader.JavaFxApplication;
-import htl.steyr.ereader.model.Customer;
-import htl.steyr.ereader.model.Operation;
-import htl.steyr.ereader.model.PublisherInterface;
-import htl.steyr.ereader.model.SubscriberInterface;
+import htl.steyr.ereader.model.*;
 import htl.steyr.ereader.util.FxUtilities;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 @Component
 public class Controller implements Initializable {
+  public ComboBox<Customer> detailsCustomer;
+  public ComboBox<Resource> detailsResource;
+  public DatePicker detailsStart;
+  public DatePicker detailsEnd;
+  public TextField detailsResourceName;
+  public TextField detailsResourceCategory;
+  public TextField detailsResourceType;
+  public ComboBox<Customer> borrowCreateCustomer;
+  public ComboBox<Resource> borrowCreateResource;
+  public DatePicker borrowCreateStart;
+  public DatePicker borrowCreateEnd;
+  public Button borrowEditConfirm;
+  public Button borrowEditCancel;
+  public Button borrowCreateButton;
+  public Button borrowCreateCancel;
+  public Button borrowReturn;
+  public TableView<Borrow> borrowTable;
+  public TableColumn<Borrow, Long> borrowTableId;
+  public TableColumn<Borrow, String> borrowTableResource;
+  public TableColumn<Borrow, Date> borrowTableStart;
+  public TableColumn<Borrow, Date> borrowTableEnd;
+  public TableColumn<Borrow, Boolean> borrowTableIsReturned;
 
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
