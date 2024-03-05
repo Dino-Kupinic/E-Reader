525fd218489ba537bb98f31677460dcf9ecdf62d
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-03-05 23:03:24 +0100

    feat: add closeWindow method

--- src/main/java/htl/steyr/ereader/util/FxUtilities.java
+++ src/main/java/htl/steyr/ereader/util/FxUtilities.java
@@ -1,7 +1,9 @@
 package htl.steyr.ereader.util;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
 
 /**
  * Utility class for JavaFX that contains utility methods.
@@ -10,18 +12,18 @@ import javafx.scene.control.MenuItem;
  */
 public class FxUtilities {
   /**
-   * Get the MenuItem text in UpperCase. (default)
+   * Get the MenuItem text.
    *
    * @param actionEvent ActionEvent
-   * @see #getMenuItemText(ActionEvent, boolean) specifying the case
    * @return menuItemText in UpperCase
+   * @see #getMenuItemText(ActionEvent, boolean) specifying the case
    */
   public static String getMenuItemText(ActionEvent actionEvent) {
-    return getMenuItemText(actionEvent, true);
+    return getMenuItemText(actionEvent, false);
   }
 
   /**
-   * Get the MenuItem text.
+   * Get the MenuItem text while providing if it should be returned UpperCase or not.
    *
    * @param actionEvent ActionEvent
    * @param inUpperCase boolean
@@ -33,5 +35,16 @@ public class FxUtilities {
       return menuItem.getText().toUpperCase();
     }
     return menuItem.getText();
+  }
+
+  /**
+   * Close the window associated with the event.
+   *
+   * @param e ActionEvent
+   */
+  public static void closeWindow(ActionEvent e) {
+    Button button = (Button) (e.getSource());
+    Stage stage = (Stage) button.getScene().getWindow();
+    stage.close();
   }
 }
