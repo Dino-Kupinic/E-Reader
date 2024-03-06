cc541f27c8f47f123736c56dd3f9f5651222e3a8
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2024-03-06 08:50:27 +0100

    feat: add CustomerRemoveController

--- src/main/java/htl/steyr/ereader/controller/customer/CustomerRemoveController.java
+++ src/main/java/htl/steyr/ereader/controller/customer/CustomerRemoveController.java
@@ -1,9 +1,15 @@
 package htl.steyr.ereader.controller.customer;
 
+import htl.steyr.ereader.model.Customer;
 import htl.steyr.ereader.model.PublisherInterface;
 import htl.steyr.ereader.model.SubscriberInterface;
 import htl.steyr.ereader.repository.CustomerRepository;
+import htl.steyr.ereader.util.FxUtilities;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -13,18 +19,38 @@ import java.util.ResourceBundle;
 @RequiredArgsConstructor
 @Component
 public class CustomerRemoveController implements Initializable, PublisherInterface {
+  public ListView<Customer> deleteCustomerList;
+  public TextField selectedText;
   private SubscriberInterface subscriber = null;
   private final CustomerRepository customerRepository;
 
-  // TODO: implement the rest
-
   @Override
-  public void addSubscriber(SubscriberInterface sub) {
-
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+    deleteCustomerList.getItems().addAll(customerRepository.findAll());
   }
 
   @Override
-  public void initialize(URL url, ResourceBundle resourceBundle) {
+  public void addSubscriber(SubscriberInterface sub) {
+    this.subscriber = sub;
+  }
 
+  public void onDeleteClicked(ActionEvent actionEvent) {
+    Customer c = deleteCustomerList.getSelectionModel().getSelectedItem();
+    if (c != null) {
+      customerRepository.delete(c);
+      subscriber.triggerAction();
+      FxUtilities.closeWindow(actionEvent);
+    }
+  }
+
+  public void onCancelClicked(ActionEvent actionEvent) {
+    FxUtilities.closeWindow(actionEvent);
+  }
+
+  public void customerListViewClicked(MouseEvent event) {
+    Customer c = deleteCustomerList.getSelectionModel().getSelectedItem();
+    if (c != null) {
+      selectedText.setText(c.toString());
+    }
   }
 }
